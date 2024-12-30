51feb5133fdc7c9b25867c96ea43f2a3eaa0a57c
Balaji Sai Charan <balajisaicharan975@gmail.com>
2024-12-30 22:13:57 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './App.css'
+import { Button } from './components/ui/button'
+import Login from './pages/login';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+    <Login/>
+    </main>
+  )
+}
+
+export default App
